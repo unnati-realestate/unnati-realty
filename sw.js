3860f4387deece5dbcb34e423a3825f4bff38f1a
unnati-realestate <unnatienterprises1118@gmail.com>
2026-08-22 00:32:30 +0530

    Fix PWA cache and navigation for Realynk v9

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE='realynk-v8';
-const SHELL=['./','./index.html','./fixed.html','./manifest.webmanifest'];
+const CACHE='realynk-v9';
+const SHELL=['./','./index.html','./fixed.html?v=9','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('realynk-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
   if(event.request.mode==='navigate'){
-    event.respondWith(fetch('./fixed.html',{cache:'no-store'}).catch(()=>caches.match('./fixed.html')));
+    event.respondWith(fetch('./fixed.html?v=9',{cache:'no-store'}).catch(()=>caches.match('./fixed.html?v=9')));
     return;
   }
-  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
+  event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request)));
 });
