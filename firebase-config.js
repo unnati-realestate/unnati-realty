6bbc3b9ad16acc18870f3f913156aad4d9e8df92
unnati-realestate <unnatienterprises1118@gmail.com>
2026-08-22 13:07:03 +0530

    Prepare Firebase web configuration for Realynk broker verification

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Realynk Firebase Web App configuration.
+// Paste the Web App config copied from Firebase Console > Project settings > Your apps.
+// Firebase web config values are intended to be public; do not put service-account/private keys here.
+export const firebaseConfig = {
+  apiKey: "PASTE_FIREBASE_WEB_API_KEY",
+  authDomain: "PASTE_FIREBASE_AUTH_DOMAIN",
+  projectId: "realynk8",
+  storageBucket: "PASTE_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "PASTE_FIREBASE_MESSAGING_SENDER_ID",
+  appId: "PASTE_FIREBASE_WEB_APP_ID"
+};
