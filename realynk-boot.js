b9461c7834e638bea9b0e17d74a8dfe8dff67d63
unnati-realestate <unnatienterprises1118@gmail.com>
2026-09-09 16:36:42 +0530

    Activate Service Worker V50 with latest Realynk flow

--- realynk-boot.js
+++ realynk-boot.js
@@ -1,8 +1,8 @@
-/* REALYNK SAFE BOOT V19 */
+/* REALYNK SAFE BOOT V20 */
 (function(){'use strict';
 function load(src,key,module){if(window[key])return;var s=document.createElement('script');s.src=src;s.async=true;if(module)s.type='module';document.head.appendChild(s)}
 function start(){
-  if('serviceWorker'in navigator)navigator.serviceWorker.register('./sw.js?v=49').catch(function(){});
+  if('serviceWorker'in navigator)navigator.serviceWorker.register('./sw.js?v=50').catch(function(){});
   load('./realynk-media.js?v=2','realynkMedia',false);
   load('./realynk-admin-entry.js?v=7','realynkAdminEntry',true);
   load('./realynk-broker-auth.js?v=2','realynkBrokerAuth',true);
