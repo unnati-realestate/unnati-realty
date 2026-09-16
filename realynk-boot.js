f00fb4536f6124181e2fd85670307bee408cd012
unnati-realestate <unnatienterprises1118@gmail.com>
2026-09-16 22:14:46 +0530

    Load separate property listing sections

--- realynk-boot.js
+++ realynk-boot.js
@@ -12,6 +12,7 @@ window.addEventListener('realynkCloudPropertiesRestored',refreshPropertyViews,fa
 window.addEventListener('realynkCloudPropertiesChanged',refreshPropertyViews,false);
 /* realynk-stability.js is the ONLY property renderer/action owner */
 load('./realynk-stability.js?v=11','realynkStabilityV11',true);
+load('./realynk-listing-sections.js?v=1','realynkListingSectionsV1',false);
 load('./realynk-cloud-unify.js?v=1','realynkCloudUnify',false);
 load('./realynk-broker-name-fix.js?v=1','realynkBrokerNameFixV1',false);
 load('./realynk-payment-core.js?v=1','realynkPaymentCoreV1',false);
