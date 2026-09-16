1e315587468d5e28e3b466e1161dc9c268fc7a7b
unnati-realestate <unnatienterprises1118@gmail.com>
2026-09-17 00:46:08 +0530

    Remove duplicate broker name on dashboard

--- realynk-boot.js
+++ realynk-boot.js
@@ -15,7 +15,6 @@ load('./realynk-stability.js?v=11','realynkStabilityV11',true);
 load('./realynk-listing-sections.js?v=1','realynkListingSectionsV1',false);
 load('./realynk-deal-fields.js?v=4','realynkDealFieldsV4',true);
 load('./realynk-cloud-unify.js?v=1','realynkCloudUnify',false);
-load('./realynk-broker-name-fix.js?v=1','realynkBrokerNameFixV1',false);
 load('./realynk-payment-core.js?v=1','realynkPaymentCoreV1',false);
 load('./realynk-payment-adapter.js?v=1','realynkPaymentAdapterV1',true);
 idle(function(){
